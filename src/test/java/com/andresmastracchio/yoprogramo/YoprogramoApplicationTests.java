fd8b42abee7b015e3fdbb6b4586c0011cf8ff35e
Andres Mastracchio <andres.mastracchio@naranjax.com>
2022-03-26 19:23:58 -0300

    Initial project commit

---
+++ src/test/java/com/andresmastracchio/yoprogramo/YoprogramoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresmastracchio.yoprogramo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoprogramoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
