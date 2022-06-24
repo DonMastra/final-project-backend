543df60bdc5340f7bd69a9e824a5c4f8d819a026
Andres Mastracchio <andres.mastracchio@naranjax.com>
2022-06-24 19:46:35 -0300

    done project use case

--- src/main/java/com/andresmastracchio/yoprogramo/usecase/impl/ProjectService.java
+++ src/main/java/com/andresmastracchio/yoprogramo/usecase/impl/ProjectService.java
@@ -1,6 +1,7 @@
 package com.andresmastracchio.yoprogramo.usecase.impl;
 
 import com.andresmastracchio.yoprogramo.adapter.repository.ProjectRepository;
+import com.andresmastracchio.yoprogramo.entity.ProfessionalExperience;
 import com.andresmastracchio.yoprogramo.entity.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,25 @@ public class ProjectService {
 
     public List<Project> getAllProjects() {
         return projectRepository.findAll();
+    }
+
+    public boolean existsByProjectTitle(String projectTitle) {
+        return projectRepository.existsByProjectTitle(projectTitle);
+    }
+
+    public Project save(Project project) {
+        return projectRepository.save(project);
+    }
+
+    public boolean existsById(Integer id) {
+        return projectRepository.existsById(id);
+    }
+
+    public void deleteById(Integer id) {
+        projectRepository.deleteById(id);
+    }
+
+    public Project getById(Integer id) {
+        return projectRepository.getById(id);
     }
 }
