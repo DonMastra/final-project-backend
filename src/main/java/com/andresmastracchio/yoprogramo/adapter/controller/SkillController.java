de0868f82375da286d6ac07da1be1a58c7418234
Andres Mastracchio <andres.mastracchio@naranjax.com>
2022-06-27 13:54:47 -0300

    closes #7 done skill controller, service & repository

--- src/main/java/com/andresmastracchio/yoprogramo/adapter/controller/SkillController.java
+++ src/main/java/com/andresmastracchio/yoprogramo/adapter/controller/SkillController.java
@@ -1,12 +1,20 @@
 package com.andresmastracchio.yoprogramo.adapter.controller;
 
+import com.andresmastracchio.yoprogramo.dto.MessageDto;
 import com.andresmastracchio.yoprogramo.entity.Skill;
 import com.andresmastracchio.yoprogramo.usecase.impl.SkillService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,57 +38,48 @@ public class SkillController {
         return new ResponseEntity<>(skills, HttpStatus.OK);
     }
 
-    // Test case controllers
-    /*
     @PostMapping("/new")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> create(@RequestBody Producto producto) {
-        if (StringUtils.isBlank(producto.getNombreProducto())) {
+    public ResponseEntity<?> create(@RequestBody Skill skill) {
+        if (StringUtils.isBlank(skill.getSkillName())) {
             return new ResponseEntity(new MessageDto("el nombre es obligatorio"), HttpStatus.BAD_REQUEST);
         }
-        if (producto.getPrecio() == 0) {
-            return new ResponseEntity(new MessageDto("el precio es obligatorio"), HttpStatus.BAD_REQUEST);
-        }
-        if (productoService.existePorNombre(producto.getNombreProducto())) {
+
+        if (skillService.existsBySkillName(skill.getSkillName())) {
             return new ResponseEntity(new MessageDto("ese nombre ya existe"), HttpStatus.BAD_REQUEST);
         }
 
-        productoService.guardar(producto);
-        return new ResponseEntity(new MessageDto("producto guardado"), HttpStatus.CREATED);
+        skillService.save(skill);
+        return new ResponseEntity(new MessageDto("skill guardada"), HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> update(@RequestBody Producto producto, @PathVariable("id") Long id) {
-        if (!productoService.existePorId(id)) {
-            return new ResponseEntity(new MessageDto("no existe ese producto"), HttpStatus.NOT_FOUND);
+    public ResponseEntity<?> update(@RequestBody Skill skill, @PathVariable("id") Integer id) {
+        if (!skillService.existsById(id)) {
+            return new ResponseEntity(new MessageDto("no existe ese skill"), HttpStatus.NOT_FOUND);
         }
-        if (StringUtils.isBlank(producto.getNombreProducto())) {
+        if (StringUtils.isBlank(skill.getSkillName())) {
             return new ResponseEntity(new MessageDto("el nombre es obligatorio"), HttpStatus.BAD_REQUEST);
         }
-        if (producto.getPrecio() == 0) {
-            return new ResponseEntity(new MessageDto("el precio es obligatorio"), HttpStatus.BAD_REQUEST);
-        }
-        if (productoService.existePorNombre(producto.getNombreProducto()) &&
-                productoService.obtenerPorNombre(producto.getNombreProducto()).get().getId() != id) {
+
+        /*if (skillService.existsBySkillName(skill.getSkillName()) &&
+                skillService.obtenerPorNombre(skill.getNombreProducto()).get().getId() != id) {
             return new ResponseEntity(new MessageDto("ese nombre ya existe"), HttpStatus.BAD_REQUEST);
-        }
-        Producto prodUpdate = productoService.obtenerPorId(id).get();
-        prodUpdate.setNombreProducto(producto.getNombreProducto());
-        prodUpdate.setPrecio(producto.getPrecio());
-        productoService.guardar(prodUpdate);
-        return new ResponseEntity(new MessageDto("producto actualizado"), HttpStatus.CREATED);
+        }*/
+
+        Skill skillUpdate = skillService.getById(id);
+        skillUpdate.setSkillName(skill.getSkillName());
+        return new ResponseEntity(new MessageDto("skill actualizado"), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
-        if (!productoService.existePorId(id)) {
-            return new ResponseEntity(new MessageDto("no existe ese producto"), HttpStatus.NOT_FOUND);
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        if (!skillService.existsById(id)) {
+            return new ResponseEntity(new MessageDto("no existe esa skill"), HttpStatus.NOT_FOUND);
         }
-        productoService.borrar(id);
-        return new ResponseEntity(new MessageDto("producto eliminado"), HttpStatus.OK);
+        skillService.deleteById(id);
+        return new ResponseEntity(new MessageDto("skill eliminada"), HttpStatus.OK);
     }
-
-    */
 }
--- src/main/java/com/andresmastracchio/yoprogramo/adapter/repository/SkillRepository.java
+++ src/main/java/com/andresmastracchio/yoprogramo/adapter/repository/SkillRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Integer> {
-    Optional<Skill> findById(int id);
+    boolean existsBySkillName(String skillName);
 }
